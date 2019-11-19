2bdc1f8aea68d2fe760e271ba96b3f65cda7f463
Lune <yari.lune@gmail.com>
2019-11-19 16:14:48 +0100

    add installation push app on click button

--- js/app.js
+++ js/app.js
@@ -18,8 +18,40 @@ butt2.addEventListener('click', function() {
 
 	navigator.serviceWorker.controller.postMessage({'clearAllCache': 'world'})
 	
+}); 
+
+let deferredPrompt;
+let btnAdd = document.querySelector('.add');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+	console.log('======================================')
+	e.preventDefault();
+
+	btnAdd.style.display = 'block';
+	// Stash the event so it can be triggered later.
+	deferredPrompt = e;
 });
 
+btnAdd.addEventListener('click', (e) => {
+	// hide our user interface that shows our A2HS button
+	btnAdd.style.display = 'none';
+	// Show the prompt
+	deferredPrompt.prompt();
+	// Wait for the user to respond to the prompt
+	deferredPrompt.userChoice
+		.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt'); alert('YOU JUST SUPER COOL APP. CONGRATULATIONS! but not yet hehe')
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		});
+});
+window.addEventListener('appinstalled', (evt) => {
+	console.log('a2hs installed');
+	alert('YOU JUST SUPER COOL APP. CONGRATULATIONS!')
+});
 
 
 
